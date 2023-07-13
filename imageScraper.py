6cb2d836c9b1b6a84f5bd3db05e5c3d8dea17233
Travis <trmail25@gmail.com>
2023-07-13 11:54:05 -0400

    added 18+ bypass

--- imageScraper.py
+++ imageScraper.py
@@ -5,9 +5,23 @@ from selenium.webdriver.common.by import By
 #gets subreddit to scrape from user
 subreddit = input("Enter subreddit to scrape: ")
 
+#opens subreddit
 driver = webdriver.Firefox()
 driver.get("https://old.reddit.com/r/" + subreddit)
 
+#check for and bypass "over 18" page
+
+if(driver.title == "reddit.com: over 18?"):
+    driver.find_element(By.XPATH,'//button[text()="continue"]').click()
+
+#create a list of comment links
+commentLinks = []
+
+commentLinks.extend(driver.find_elements(By.PARTIAL_LINK_TEXT, "comment"))
+
+for link in commentLinks:
+    print(link.get_attribute("href"))
+    print("\n")
 
 
 #elem = driver.find_element(By.NAME, "q")
