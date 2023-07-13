a6800baaf6ee2cfa76d8709d2fba98894e5d1ec6
Travis <trmail25@gmail.com>
2023-07-13 11:34:29 -0400

    added prompt to choose subreddit made webdriver load shosen subreddit

--- imageScraper.py
+++ imageScraper.py
@@ -2,11 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+#gets subreddit to scrape from user
+subreddit = input("Enter subreddit to scrape: ")
+
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("https://old.reddit.com/r/" + subreddit)
+
+
+
+#elem = driver.find_element(By.NAME, "q")
+#elem.clear()
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
