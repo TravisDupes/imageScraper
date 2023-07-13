ebf4aac4cb670f243d85f833d263818fe4952eb5
Travis <trmail25@gmail.com>
2023-07-13 12:47:17 -0400

    changed comment link scraper to be recursive added functionality to create a save directory

--- imageScraper.py
+++ imageScraper.py
@@ -1,9 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import os
+import datetime
+
+
+def recursiveCommentLinkScrape(driver):
+    #create a list of comment comment Links
+    commentLinks = []
+
+    commentElements = driver.find_elements(By.PARTIAL_LINK_TEXT, "comment")
+
+    if len(commentElements) > 0:
+        for commentElement in commentElements:
+            commentLinks.append(commentElement.get_attribute("href"))
+
+    nextButton = driver.find_elements(By.LINK_TEXT, "next ›")
+
+    if len(nextButton) > 0:
+        nextButton[0].click()
+        return(commentLinks + recursiveCommentLinkScrape(driver))
+
+    return(commentLinks)
 
 #gets subreddit to scrape from user
 subreddit = input("Enter subreddit to scrape: ")
+
+saveDirectory = "ImageScraper/" + subreddit + datetime.datetime.now().strftime("%Y%m%d")
+
+if not os.path.exists(saveDirectory):
+    os.makedirs(saveDirectory)
+
 
 #opens subreddit
 driver = webdriver.Firefox()
@@ -14,13 +41,17 @@ driver.get("https://old.reddit.com/r/" + subreddit)
 if(driver.title == "reddit.com: over 18?"):
     driver.find_element(By.XPATH,'//button[text()="continue"]').click()
 
-#create a list of comment links
+
 commentLinks = []
 
-commentLinks.extend(driver.find_elements(By.PARTIAL_LINK_TEXT, "comment"))
+commentLinks.extend(recursiveCommentLinkScrape(driver))
+
+
+
+
 
 for link in commentLinks:
-    print(link.get_attribute("href"))
+    print(link)
     print("\n")
 
 
